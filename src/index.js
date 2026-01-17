bcdd067b49591eb2025e5f8c2cb24ef641ddb678
LeoPavlovski <leopavlovski01@gmail.com>
2026-01-17 13:07:03 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// We used this for the pure-react  :
+// <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+// <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+// Same thing.
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// conventional to name the function App
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+// Create root
+// React v18
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  //   Renders component twice to find certain bugs.
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Before before 18
+// React.render(<App/>);
